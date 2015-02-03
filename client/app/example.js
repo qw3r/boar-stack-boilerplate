6aa6ad06456da0e171cbef6b01556e48c5a7c0c5
Viktor Somodi <somodi.viktor@gmail.com>
2015-02-03 14:26:51 +0100

    add 6to5 support

--- client/app/example.js
+++ client/app/example.js
@@ -1,4 +1,19 @@
 'use strict';
 
 var moment = require('moment');
-console.log('It is a beautiful '+ moment().format('dddd'));
+console.log('It is a beautiful '+ moment().format('dddd') + 'yo');
+
+class View {
+  constructor(options) {
+    this.model = options.model;
+    this.template = options.template;
+  }
+
+  log() {
+    console.log('1');
+  }
+}
+
+
+var v = new View();
+v.log();
