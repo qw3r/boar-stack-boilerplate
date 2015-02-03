517f2716df985e193e3967a148daf53d393535b9
Viktor Somodi <somodi.viktor@gmail.com>
2015-02-03 10:31:32 +0100

    log real ip address

--- server/controllers/main/actions/list/get.html.js
+++ server/controllers/main/actions/list/get.html.js
@@ -1,8 +1,13 @@
 'use strict';
 
+var realIpAddress = require('../../../../lib/real-ip-address/index');
+var logger = require('logentries-logformat')('main');
+
+
 var GetListAction = function(context) {
   this.context = context;
   this.params = context.params;
+  this.request = context.request;
 };
 
 
@@ -11,6 +16,8 @@ GetListAction.prototype = {
   process: function* () {
     var message = 'papaja';
     if (this.params && this.params.fromRequestBody) message += this.params.fromRequestBody;
+
+    logger.log('mainActionUsed', { userIp: realIpAddress(this.request) });
 
     yield this.context.render('index', { hello: message });
   }
