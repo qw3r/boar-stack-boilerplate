468011402700d925dd3edb437a325a8ec2b22654
Viktor Somodi <somodi.viktor@gmail.com>
2015-02-04 09:36:27 +0100

    refactor admin list json action

--- server/controllers/main/actions/list/get.json.js
+++ server/controllers/main/actions/list/get.json.js
@@ -1,9 +1,30 @@
 'use strict';
 
-module.exports = function* () {
+var realIpAddress = require('../../../../lib/real-ip-address/index');
+var logger = require('logentries-logformat')('main');
 
-  this.body = {
-    admins: ['superman', 'batman', 'aquaman', 'flash']
-  };
+
+var GetListJsonAction = function(context) {
+  this.context = context;
+  this.request = context.request;
+};
+
+
+GetListJsonAction.prototype = {
+
+  process: function* () {
+    logger.log('getListJson', { userIp: realIpAddress(this.request) });
+
+    this.context.body = {
+      admins: ['superman', 'batman', 'aquaman', 'flash']
+    };
+  }
 
 };
+
+
+module.exports = function* () {
+  var action = new GetListJsonAction(this);
+  yield action.process();
+};
+
