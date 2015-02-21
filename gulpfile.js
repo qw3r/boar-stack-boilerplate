aea4a120247302bac004e0e29af1c40c07b13fbc
blacksonic <soos.gabor86@gmail.com>
2015-02-21 12:37:23 +0100

    export every task

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,16 @@
 'use strict';
 
 var gulp = require('gulp');
-var del = require('del');
-var runSequence = require('run-sequence');
 var config = require('./tasks/config');
 var server = require('./tasks/server')(gulp, config);
 var client = require('./tasks/client')(gulp, config);
 var e2e = require('./tasks/e2e')(gulp, config);
 
-// Public Tasks
-gulp.task('build', ['build-clean'], function(cb) {
-  runSequence([
-    'server-copy', 'client-build'
-  ], cb);
-});
+var options = {
+  config: config,
+  client: client,
+  server: server,
+  e2e: e2e
+};
 
-gulp.task('start', ['build'], function() {
-  gulp.run('server');
-  gulp.run('server-watch');
-  gulp.run('client-watch');
-});
-
-gulp.task('test', ['server-test', 'client-test']);
-
-
-
-// Helper
-gulp.task('build-clean', function(cb) {
-  del([config.build.distPath + '**/*'], cb);
-});
-
-
-
-// Server Tasks
-gulp.task('server', server.start);
-gulp.task('server-copy', function() { return server.copy(false); });
-gulp.task('server-copy-only-changed', function () {
-  return server.copy(true);
-});
-gulp.task('server-jshint', function() { return client.jshint(); });
-gulp.task('server-watch', function() {
-  gulp.watch(config.server.filePattern, ['server-copy-only-changed']);
-});
-gulp.task('server-test', server.test);
-
-
-
-// Client Tasks
-gulp.task('client-build', [
-  'client-build-static',
-  'client-build-vendor',
-  'client-build-scripts',
-  'client-build-stylesheets',
-  'client-build-views'
-]);
-gulp.task('client-build-static', function () { return client.copyStatic(); });
-gulp.task('client-build-vendor', function() { return client.concatVendors(); });
-gulp.task('client-build-vendor-alt', function() { return client.buildVendors(); });
-gulp.task('client-build-scripts', function() { return client.buildScripts(); });
-gulp.task('client-build-stylesheets', function() { return client.buildStylesheets(); });
-gulp.task('client-build-views', function() { return client.buildViews(); });
-gulp.task('client-jshint', function() { return client.jshint(); });
-
-gulp.task('client-watch', function() {
-  gulp.watch(config.client.static.watchPattern, ['client-build-static']);
-  gulp.watch(config.client.app.watchPattern, ['client-build-scripts']);
-  gulp.watch(config.client.stylesheets.watchPattern, ['client-build-stylesheets']);
-  gulp.watch(config.client.app.viewPattern, ['client-build-views']);
-});
-
-gulp.task('client-test', client.test);
-
-// End to End Tasks
-gulp.task('e2e-test', e2e.test);
-gulp.task('update-webdriver', e2e.updateWebDriver);
+require('./tasks/setup')(gulp, options);
--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-module.exports = {
-  config: require('./tasks/config'),
-  client: require('./tasks/client'),
-  server: require('./tasks/server'),
-  e2e: require('./tasks/e2e')
+'use strict';
+var merge = require('merge');
+var config = require('./tasks/config');
+var client = require('./tasks/client');
+var server = require('./tasks/server');
+var e2e = require('./tasks/e2e');
+var setup = require('./tasks/setup');
+
+module.exports = function(customConfig) {
+  var finalConfig = merge.recursive(true, config, customConfig);
+
+  return {
+    config: finalConfig,
+    client: function(gulp) {
+      return client(gulp, finalConfig);
+    },
+    server: function(gulp) {
+      return server(gulp, finalConfig);
+    },
+    e2e: function(gulp) {
+      return e2e(gulp, finalConfig);
+    },
+    setup: function(gulp) {
+      return setup(gulp, {
+        config: finalConfig,
+        client: client(gulp, finalConfig),
+        server: server(gulp, finalConfig),
+        e2e: e2e(gulp, finalConfig)
+      });
+    }
+  };
 };
