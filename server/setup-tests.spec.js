0301c1285195a79bb79a897e16e25b0c9ccf37dc
Viktor Somodi <somodi.viktor@gmail.com>
2015-02-28 08:07:26 +0100

    indent 2 in setup tests

--- server/setup-tests.spec.js
+++ server/setup-tests.spec.js
@@ -1,38 +1,35 @@
 'use strict';
 
 var sinon = require('sinon');
-var renderSpy = require('js-stack').mocks.render;
-
+var renderSpy = require('js-stack.mocks').render;
+var chai = require('chai');
+var sinonChai = require('sinon-chai');
+var chaiSubset = require('chai-subset');
+var chaiAsPromised = require('chai-as-promised');
 
 before(function () {
-    var chai = require('chai');
-    var sinonChai = require('sinon-chai');
-    var chaiSubset = require('chai-subset');
-    var chaiAsPromised = require('chai-as-promised');
+  chai.use(chaiAsPromised);
+  chai.use(chaiSubset);
+  chai.use(sinonChai);
 
-    chai.use(chaiAsPromised);
-    chai.use(chaiSubset);
-    chai.use(sinonChai);
+  sinon.stub.returnsWithResolve = function (data) {
+    return this.returns(Promise.resolve(data));
+  };
 
-    sinon.stub.returnsWithResolve = function (data) {
-        return this.returns(Promise.resolve(data));
-    };
-
-    sinon.stub.returnsWithReject = function (error) {
-        return this.returns(Promise.reject(error));
-    };
-
+  sinon.stub.returnsWithReject = function (error) {
+    return this.returns(Promise.reject(error));
+  };
 });
 
 
 beforeEach(function () {
-    this.sandbox = sinon.sandbox.create();
-    this.renderSpy = renderSpy;
-    this.renderSpy.attach();
+  this.sandbox = sinon.sandbox.create();
+  this.renderSpy = renderSpy;
+  this.renderSpy.attach();
 });
 
 
 afterEach(function () {
-    this.sandbox.restore();
-    this.renderSpy.restore();
+  this.sandbox.restore();
+  this.renderSpy.restore();
 });
