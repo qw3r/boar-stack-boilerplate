c26a090043a0913455d63e60938db55ad3e80bc3
Viktor Somodi <somodi.viktor@gmail.com>
2015-03-06 11:07:31 +0100

    use co-supertest instead of supertest

--- server/controllers/main/actions/healthcheck/get.spec.js
+++ server/controllers/main/actions/healthcheck/get.spec.js
@@ -1,32 +1,32 @@
 "use strict";
 
 var server = require('../../../../server');
-var request = require('supertest');
+var request = require('co-supertest');
 var Healthcheck = require('../../../../models/healthcheck');
 
 describe('Main Controller Healthceck GET Action', function() {
 
-  it('should respond with proper message and code if everything went fine', function(done) {
-    request(server.listen())
+  it('should respond with proper message and code if everything went fine', function* () {
+    yield request(server.listen())
       .get('/healthcheck')
       .expect(200)
       .expect('Content-Type', /json/)
       .expect({success: true})
-      .end(done);
+      .end();
   });
 
 
-  it('should respond with failed message and code if something happened in db', function(done) {
+  it('should respond with failed message and code if something happened in db', function* () {
     this.sandbox.stub(Healthcheck, 'find', function(cb) {
       cb({}, []);
     });
 
-    request(server.listen())
+    yield request(server.listen())
       .get('/healthcheck')
       .expect(500)
       .expect('Content-Type', /json/)
       .expect({success: false})
-      .end(done);
+      .end();
   });
 
 
