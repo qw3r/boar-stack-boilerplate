ae85f0faa5befb8612909e722a3c49e6b93969b6
Viktor Somodi <somodi.viktor@gmail.com>
2015-04-09 21:32:01 +0200

    fix test jshint error

--- client/app/example.js
+++ client/app/example.js
@@ -4,7 +4,7 @@ var angular = require('angular');
 var Controller = require('./controllers/example');
 
 angular
-  .module("boilerplate", ['templates'])
+  .module('boilerplate', ['templates'])
   .controller('ExampleController', ['$http', Controller])
   .run(($http) => {
     $http.defaults.headers.get = {'Accept': 'application/json'};
