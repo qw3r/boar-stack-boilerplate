df62ace664529a8bb2e9dd3c70a07ef9feea5b62
Viktor Somodi <somodi.viktor@gmail.com>
2015-04-09 09:40:19 +0200

    add angular to client and client test vendors

--- tasks.config.js
+++ tasks.config.js
@@ -4,5 +4,13 @@ module.exports = {
       PORT: process.env.PORT || 9100,
       BASE_URL: process.env.BASE_URL || 'http://localhost:9100'
     }
+  },
+  client: {
+    vendors: [
+      'node_modules/angular/angular.js'
+    ],
+    app: {
+      testModules: ['node_modules/angular-mocks/angular-mocks.js']
+    }
   }
 };
